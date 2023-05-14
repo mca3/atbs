c1a711c373eb0808ce8aa036aaf44e76f0512509
mca <mca@int21h.xyz>
2023-05-14 17:09:55 -0300

    readme update & baud config

--- README.md
+++ README.md
@@ -2,7 +2,47 @@
 
 atbs (AT Bootstrap) is a tool to bootstrap PPP connections (or whatever else)
 over serial, useful for connecting ancient machines to the Internet like it's
-1990.
+the late 1990s.
 
 atbs emulates just enough of a Hayes-compatible modem to dial a number and
-establish a connection to say, a PPP server.
+establish a connection to, say, a PPP server.
+atbs has been successfully used to do just that on a physical Windows XP
+machine.
+
+## Usage
+
+Download and compile, there are no dependencies.
+It is known to work on Linux.
+
+atbs takes arguments that control how it works and what settings it uses.
+You will likely be interested in the following:
+
+- `-b`: baud rate (default: 9600)
+- `-c`: command (default: `ppp notty`)
+- `-t`: serial device (default: `/dev/tty`, current tty)
+
+Once started and connected to a computer over serial, you can connect to the
+modem and dial any number to execute the command you specified.
+
+## Known issues
+
+- Incomplete modem emulation
+
+As atbs is in a very early stage in its development, just enough has been
+written to connect to the Internet.
+Modem escapes are not supported, and as such it is impossible to disconnect.
+Many, many commands are not supported.
+
+Additionally, control lines have largely been left unimplemented, and it is
+unknown if the little code written for them even works properly.
+
+- Little instructions
+
+I'll write a guide on how to get a PPP server up and running with this
+eventually, but for now you're left on your own.
+
+- Largely untested
+
+The only testing I have done with this has me plugging it into a Windows XP
+machine and hoping for the best; while it hasn't let me down yet, it will most
+certainly let you down if you aren't using it exactly like how I am.
--- main.c
+++ main.c
@@ -11,7 +11,7 @@ extern int optind, opterr, optopt;
 void
 usage(char *argv0)
 {
-	fprintf(stderr, "usage: %s [-b baud] [-t /dev/tty]\n", argv0);
+	fprintf(stderr, "usage: %s [-b baud] [-t /dev/tty] [-c command]\n", argv0);
 	exit(EXIT_FAILURE);
 }
 
@@ -23,27 +23,53 @@ main(int argc, char *argv[])
 	int baud = 9600;
 	speed_t actual_baud = B9600;
 	struct modem_state m = {0};
+	m.command = "pppd notty";
 
-	while ((opt = getopt(argc, argv, "b:t:")) != -1) {
+	while ((opt = getopt(argc, argv, "b:c:t:")) != -1) {
 		switch (opt) {
-		case 'b': baud = atoi(optarg); break;
+		case 'b': baud = m.baud = atoi(optarg); break;
+		case 'c': m.command = optarg; break;
 		case 't': tty = optarg; break;
 		default: usage(argv[0]);
 		}
 	}
 
 	switch (baud) {
+	case 50: actual_baud = B50; break;
+	case 75: actual_baud = B75; break;
+	case 110: actual_baud = B110; break;
+	case 134: actual_baud = B134; break;
+	case 150: actual_baud = B150; break;
+	case 200: actual_baud = B200; break;
+	case 300: actual_baud = B300; break;
+	case 600: actual_baud = B600; break;
+	case 1200: actual_baud = B1200; break;
+	case 1800: actual_baud = B1800; break;
+	case 2400: actual_baud = B2400; break;
+	case 4800: actual_baud = B4800; break;
 	case 9600: actual_baud = B9600; break;
-	default: fprintf(stderr, "invalid baud: %d\n", baud); exit(EXIT_FAILURE);
+	case 19200: actual_baud = B19200; break;
+	case 38400: actual_baud = B38400; break;
+	case 57600: actual_baud = B57600; break;
+	case 115200: actual_baud = B115200; break;
+	case 230400: actual_baud = B230400; break;
+	case 460800: actual_baud = B460800; break;
+	case 500000: actual_baud = B500000; break;
+	case 576000: actual_baud = B576000; break;
+	case 921600: actual_baud = B921600; break;
+	case 1000000: actual_baud = B1000000; break;
+	case 1152000: actual_baud = B1152000; break;
+	case 1500000: actual_baud = B1500000; break;
+	case 2000000: actual_baud = B2000000; break;
+	default: fprintf(stderr, "invalid baud rate: %d\n", baud); exit(EXIT_FAILURE);
 	}
 
 	m.echo = 1;
 	m.verbose = 1;
 	m.registers[AT_REG_TERM] = '\r';
-	m.command = "sh";
 	m.baud = baud;
 
-	serial_new(&m.ser, tty, baud);
+	serial_new(&m.ser, tty, actual_baud);
 	serial_set_echo(&m.ser, 1);
 
 	for (;;)
